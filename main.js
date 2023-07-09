468b8cd569aca588ccfdeb62269718a7d21f8694
Heleir <itzelvargas2002@gmail.com>
2023-07-08 20:08:14 -0500

    Correccion de detalles en medidas y hover

--- main.js
+++ main.js
@@ -10,61 +10,77 @@ $(function(){
 });
 
 //Busqueda y validacion de inputs
-// const inputs = document.querySelectorAll("input");
+const inputs = document.querySelectorAll("input");
 
-// inputs.forEach(input =>{
-//     input.addEventListener("blur", (input) => {
-//         valida(input.target);
-//     })
-// })
+inputs.forEach(input =>{
+    input.addEventListener("blur", (input) => {
+        valida(input.target);
+    })
+})
 
-// function valida(input){
-//     const tipoDeInput = input.dataset.tipo;
-//     if(validadores[tipoDeInput]){
-//         validadores[tipoDeInput](input);
-//     }
+function valida(input){
+    const tipoDeInput = input.dataset.type;
+    // if(validadores[tipoDeInput]){
+    //     validadores[tipoDeInput](input);
+    // }
     
-//     if (input.validity.valid){
-//         input.classList.remove("input--invalid");
-//         input.querySelector(".input-message--error").innerHTML = "";
-//     }else{
-//         input.classList.add("input--invalid");
-//         input.querySelector(".input-message--error").innerHTML = mostrarMensajeDeErrror(tipoDeInput, input);
-//     }
+    if (input.validity.valid){
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".message-error").innerHTML = "";
+    }else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".message--error").innerHTML = mostrarMensajeDeErrror(tipoDeInput, input);
+    }
 
-// }
+}
 
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError"
+];
 
-// const tipoDeErrores = [
-//     "valueMissing",
-//     "typeMismatch",
-//     "patternMismatch",
-//     "customError"
-// ];
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacio",
+    },
+    email: {
+        valueMissing: "Este campo no puede estar vacio",
+        typeMismatch: "El correo no es válido"
+    },
+    password: {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "Minimo 8 caracteres, al menos una letra, un número y un caracter especial"
+    },
+    url: {
+        valueMissing: "La url no puede estar vacia",
+        typeMismatch: "Agrega una URL válida"
+    },
+    categoria: {
+        valueMissing: "Agrega una categoria"
+    },
+    producto: {
+        valueMissing: "Agrega el nombre del producto"
+    },
+    precio: {
+        valueMissing: "Agrega un precio"
+    }
 
-// const mensajesDeError = {
-//     email: {
-//         valueMissing: "Este campo no puede estar vacio",
-//         typeMismatch: "El correo no es válido"
-//     },
-//     password: {
-//         valueMissing: "Este campo no puede estar vacio",
-//         patternMismatch: "Minimum eight characters, at least one uppercase letter, one lowercase letter and one number"
-//     },
-// }
+}
 
+function mostrarMensajeDeErrror(tipoDeInput, input){
+    let mensaje ='';
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]){
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            // console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    })
+    return mensaje;
+ }
 // const validadores = {
 //     nacimiento: (input) => validarNacimiento(input),
 // };
-
-// function mostrarMensajeDeErrror(tipoDeInput, input){
-//     let mensaje ='';
-//     tipoDeErrores.forEach((error) => {
-//         if (input.validity[error]){
-//             console.log(tipoDeInput, error);
-//             console.log(input.validity[error]);
-//             console.log(mensajesDeError[tipoDeInput][error]);
-//             mensaje = mensajesDeError[tipoDeInput][error];
-//         }
-//     })
-// }
